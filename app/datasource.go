ffb54bbb2fb34eec9db82de973a3c2f743254c69
MavHIDE <filinnaxui@yandex.ru>
2020-09-21 20:46:08 +0500

    Add authorization

--- app/datasource.go
+++ app/datasource.go
@@ -15,20 +15,26 @@ type UserDB struct {
 
 func FindUserByFirstNameAndLatName(firstName string, lastName string) *UserDB {
 	db, err := sql.Open("sqlite3", "./db/dev.db")
-	checkError(err)
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer db.Close()
 
 	var user UserDB
 	row := db.QueryRow("SELECT * FROM user WHERE first_name = $1 AND last_name = $2", firstName, lastName)
 	err = row.Scan(&user.id, &user.firstName, &user.lastName, &user.telegramId)
-	checkError(err)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	return &user
 }
 
 func SetTelegramIdToUser(user *UserDB, telegramId int) bool {
 	db, err := sql.Open("sqlite3", "./db/dev.db")
-	checkError(err)
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer db.Close()
 
 	result, err := db.Exec("UPDATE user SET telegram_id = $1 WHERE id = $2", telegramId, user.id)
@@ -41,19 +47,17 @@ func SetTelegramIdToUser(user *UserDB, telegramId int) bool {
 
 func FindUserByTelegramId(telegramId int) *UserDB {
 	db, err := sql.Open("sqlite3", "./db/dev.db")
-	checkError(err)
+	if err != nil {
+		fmt.Println(err)
+	}
 	defer db.Close()
 
 	var user UserDB
 	row := db.QueryRow("SELECT * FROM user WHERE telegram_id = $1", telegramId)
 	err = row.Scan(&user.id, &user.firstName, &user.lastName, &user.telegramId)
-	checkError(err)
-
-	return &user
-}
-
-func checkError(err error) {
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	return &user
 }
